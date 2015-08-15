849065a7ec4b0b9b5957e9e9150da56ba62f2261
Jim Kang <jimkang@gmail.com>
2015-08-15 09:34:46 -0400

    Updated cache client to respond to RPC disconnect.

--- cached-named-levels-class.js
+++ cached-named-levels-class.js
@@ -3,7 +3,13 @@ var multilevelCacheTools = require('multilevel-cache-tools');
 
 var memoizedFn = multilevelCacheTools.client.memoize({
   fn: getNamedLevelsClass,
-  port: 4848
+  port: 4848,
+  onConnectError: respondToCacheDisconnect
 });
+
+function respondToCacheDisconnect() {
+  console.log('Cache disconnected! namedlevels-api exiting.');
+  process.exit();
+}
 
 module.exports = memoizedFn;
